faca2971ec303fc5227a31808fc3e0a95e4843a9
whoisreo <maksim03999@gmail.com>
2024-10-17 19:32:42 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,14 +1,14 @@
-n = int(input("Введите число (от 3 до 20): "))
-pairs = set()
+number = int(input("Введите число (от 3 до 20): "))
+password = []
 
-if 3 <= n <= 20:
-    for i in range(1, 21):
-        for j in range(1, 21):
-            if n == (i + j) or (n % (i + j) == 0 and i != j):
-                pairs.add((min(i, j), max(i, j)))
 
-    pair_list = [num for pair in pairs for num in pair]
+if 3 <= number < 21:
+    for i in range(1, number - 1):
+        for j in range(i+1, number):
+            if number % (i + j) == 0:
+                password.append(i)
+                password.append(j)
 
-    print(n, "=", *pair_list)
-else:
-    print("GG")
+
+print(*password)
+
