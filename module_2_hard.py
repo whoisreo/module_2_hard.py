ec60d4bd9476f239385fd7b68013f040fa0862d7
whoisreo <maksim03999@gmail.com>
2024-10-17 19:36:08 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -4,7 +4,7 @@ password = []
 
 if 3 <= number < 21:
     for i in range(1, number - 1):
-        for j in range(i+1, number):
+        for j in range(i+1, number - 1):
             if number % (i + j) == 0:
                 password.append(i)
                 password.append(j)
