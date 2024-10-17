45222c6c10a93ddd6bf180cf4c32823cf81e9b2b
whoisreo <maksim03999@gmail.com>
2024-10-17 19:37:09 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -2,7 +2,7 @@ number = int(input("Введите число (от 3 до 20): "))
 password = []
 
 
-if 3 <= number < 21:
+if 2 < number < 21:
     for i in range(1, number - 1):
         for j in range(i+1, number - 1):
             if number % (i + j) == 0:
